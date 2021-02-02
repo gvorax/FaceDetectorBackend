e878be7a1d79552270ca2cfa1ef138b5cec1f979
Otabek Ulashov <otabek83019@gmail.com>
2021-02-03 02:07:42 +0500

    changing database

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const knex = require('knex')
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'gvorax123',
-      database : 'smart-brain'
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
     }
  }); 
 db.select('*').from('users');
